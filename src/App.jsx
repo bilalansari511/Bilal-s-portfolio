ab9de80c85e079e675c28362a66857eadc08df4f
bilalansari511 <bilalansari29476@gmail.com>
2025-12-27 21:16:02 +0530

    Add About, Projects, and Contact components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Layout from './Layout'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import Contact from './Components/Contact'
 
 function App() {
   
   return (
     <>
    <Layout/>
+   <About/>
+   <Projects/>
+   <Contact/>
     </>
   )
 }
